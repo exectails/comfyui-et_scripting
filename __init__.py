14e425d523f6bc80aa118a3db08566603f357d56
exectails <exec@dragonbyte.de>
2024-11-26 12:40:25 +0100

    Standardized node list

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from .nodes.scripting import ETPythonTextScript3Node
 
-et_nodes = [
-    ("Python Text Script (3 Arguments)", ETPythonTextScript3Node),
-]
+NODE_CLASS_MAPPINGS = {
+    "ETPythonTextScript3Node": ETPythonTextScript3Node,
+}
 
-NODE_CLASS_MAPPINGS = {cls.__name__: cls for display_name, cls in et_nodes}
-NODE_DISPLAY_NAME_MAPPINGS = {cls.__name__: display_name for display_name, cls in et_nodes}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ETPythonTextScript3Node": "Python Text Script (3 Arguments)",
+}
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
